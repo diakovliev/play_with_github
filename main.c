f5acc29b4cc5d3ca5eef012fece4eee4ef79e35f
Dmytro Iakovliev <D.Iakovliev@adbglobal.com>
2014-07-03 11:25:05 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ *	Project: <project>
+ *
+ *	File: <filename>
+ *	Author: <author>
+ *	Created: <created date>
+ *
+ *	Description:
+ *
+ *
+ */
+#include <stdio.h>
+
+int main(int argc, char **argv) 
+{
+	printf("Hello\r\n");
+	return 0;
+}
